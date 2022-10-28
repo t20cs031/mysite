40faa37071d57367e55c409495b03800b8f8ef68
Kazuhito TERADA <t20cs031@yamanashi.ac.jp>
2022-10-28 09:13:47 +0900

    はじめてのDjangoアプリ作成その4

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 from django.http import Http404
-from .models import Question
+from .models import Choice,Question
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+     
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -22,8 +41,30 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        """
+        request.POSTはキーを指定することで，送信したデータにアクセスできる．この場合は，
+        選択されたIDを文字列として返す．request.POSTの値は常に文字列であるため注意．
+        """
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) # 該当questionオブジェクトを取得
+    except (KeyError, Choice.DoesNotExist): # choiceが得られなかった場合に投げられるエラー
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # POSTメソッドが成功した後は常にHttpResponseRedirectコンストラクタの中のreverse関数を使用する
+        # reverse関数により，ビュー関数中でのURLのバーコードを防ぐことができる
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
